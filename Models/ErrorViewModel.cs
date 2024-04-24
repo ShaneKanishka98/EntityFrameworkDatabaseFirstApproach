ea2b5e6cccf9e57a214c62b7d31e038496e984b6
ShaneKanishka98 <shanekanishka1998@gmail.com>
2024-04-24 17:23:51 +0530

    DB First Approach

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace coreDBFirstApproach.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
